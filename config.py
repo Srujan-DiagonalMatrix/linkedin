efe72ab1dfb34fcf768e333db4b5b07f16d6ba3d
DiagonalMatrix <srujan.crm@gmail.com>
2019-10-08 16:21:52 +0100

    test

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # Chrome Driver PATH
 chrome_driver_path = "/home/elonmusk/Documents/linkedin_run/chromedriver"
-input_file = "/home/elonmusk/Documents/github/linkedin_run/I_O/message_test_input.csv"
+#input_file = "/home/elonmusk/Documents/github/linkedin_run/I_O/message_test_input.csv"
 
-total_li_user = 2
+total_li_user = 1
 
 # User 1
-li_username_1 = "srujan@diagonalmatrix.com"
-li_password_1 = "Dmatrix1"
+li_username_1 = "srujan@diagonalmatrix.co.uk"
+li_password_1 = "revolve1"
 
 # User 2
-li_username_2 = "srujan@diagonalmatrix.co.uk"
-li_password_2 = "revolve1"
+li_username_2 = "srujan@diagonalmatrix.com"
+li_password_2 = "Dmatrix1"
 
 # User 3
 #li_username_3 = "swetha4msc@gmail.com"
