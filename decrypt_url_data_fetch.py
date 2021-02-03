0df675d7f06ae0f8c8bf4cfcf2c94d2db8fe62cf
izaq3103 <izaq786@gmail.com>
2021-02-03 08:33:27 -0700

    new change

--- decrypt_url_data_fetch.py
+++ decrypt_url_data_fetch.py
@@ -48,57 +48,75 @@ class Connect:
     @staticmethod
     def search_results(url):
         try:
-            li_url = "https://www.linkedin.com/sales/people/{}".format(url)
+            li_url = url
             driver.get(li_url)
-            time.sleep(20)
-        except:
+            time.sleep(15)
+        except Exception as e:
             print('ERROR: Invalid URL : ---------- ' + li_url)
             print(driver.current_url)
             pass
 
     @staticmethod
-    def get_contact_link():
+    def get_contact_link(url):
         try:
             time.sleep(5)
             action_menu_selector = 'div[class="artdeco-dropdown artdeco-dropdown--placement-bottom artdeco-dropdown--justification-right ember-view"] > button'
             driver.find_element_by_css_selector(action_menu_selector).click()
-            time.sleep(5)
+            time.sleep(7)
             driver.find_element_by_css_selector('div[data-control-name="copy_linkedin"]').click()
-            time.sleep(5)
+            time.sleep(6)
             li_link = pyperclip.paste()
             print(li_link)
             return li_link
         except Exception as e:
-            raise Exception("Problem in getting the Linkedin url for sales contact")
+            return f"Problem in getting the Linkedin url for ::{url}"
 
     @staticmethod
-    def get_second_degree_contact():
+    def get_second_degree_contact(url):
         try:
             time.sleep(5)
             element = driver.find_element_by_css_selector('span[class="label-16dp block"]')
             print(element.text)
-            if element.text == '2nd':
-                time.sleep(5)
-                connection_text = driver.find_element_by_css_selector('dd[class="t-12 t-black--light t-normal"]').text
-                return connection_text.split("know")[1]
-            else:
-                return element.text
+            try:
+                common_text = driver.find_element_by_css_selector('div.incommon-entity > dl > dd').text
+                entity = driver.find_element_by_css_selector('div.incommon-entity > dl > dt').text
+                if 'groups' in entity:
+                    return common_text.split("in the")[1].strip()
+                elif 'connections' in entity:
+                    return common_text.split("know")[1].strip()
+            except Exception as e:
+                pass
+            try:
+                element1 = driver.find_element_by_css_selector('li[class="incommon profile-highlight-card relative overflow-hidden inline-block mr4"] > p')
+                print(f"{element1.text} for {url}")
+                return element.text.strip()
+            except Exception as e:
+                pass
         except Exception as e:
-            print("Problem in getting the degree of a contact")
+            return f"Problem in getting the degree for::{url}"
+
+    @staticmethod
+    def get_connections(url):
+        try:
+            element = driver.find_element_by_css_selector("div[class='profile-topcard__connections-data type-total inline t-14 t-black--light mr5']")
+            return element.text.split(" ")[0].strip()
+        except Exception as e:
+            return f"Problem in finding the connections for::{url}"
 
 
 if __name__ == '__main__':
     cn = Connect()
     for url in range(len(linkedin_urls)):
         cn.search_results(linkedin_urls[url])
-        li_link = cn.get_contact_link()
-        degree = cn.get_second_degree_contact()
-        dict_data = {'Contact Url': li_link, 'Mutuals Contacts': degree}
+        li_link = cn.get_contact_link(linkedin_urls[url])
+        degree = cn.get_second_degree_contact(linkedin_urls[url])
+        connections = cn.get_connections(linkedin_urls[url])
+        dict_data = {'Contact Url': li_link, 'Mutuals Contacts': degree, 'Connections': connections}
         csv_data.append(dict_data)
     driver.quit()
 
     try:
-        fields = ['Contact Url', 'Mutuals Contacts']
+        fields = ['Contact Url', 'Mutuals Contacts', 'Connections']
         with open(csv_w_filepath, 'a') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fields)
             writer.writeheader()
