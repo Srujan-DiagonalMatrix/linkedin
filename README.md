982294886662504c625ef43b5976ce76b747fdec
izaq3103 <izaq786@gmail.com>
2021-02-06 00:10:50 -0700

    New Code

--- README.md
+++ README.md
@@ -11,10 +11,36 @@ The LinkedIn script `follow_li.py` will send a invitation for each user passed i
 4) Install `chromedriver`. Mac OS: `brew install chromedriver`. Linux: http://stackoverflow.com/a/24364290
 
 5) Execute scripts: 
+About decrypt_url_data_fetch.py::
+decrypt_url_data_fetch.py fetches data for sales person linked url, mutual groups/contact or degree and his number of connections.
+
+How to run the decrypt_url_data_fetch.py?
+
+Command to fetch all the data at once:
+python decrypt_url_data_fetch.py -u -d -c
+
+-u stands for Sales person linked url. True if present
+-d stands for mutual groups/contact or degree
+-c stands for number of Connections
+
+However, there some constraint while fetching this data in different ways or at different times which are mentioned below:
+* You can either fetch all the data or just Sales person linked url at once.
+Command to run> python decrypt_url_data_fetch.py -u -d -c
+               or
+Command to run> python decrypt_url_data_fetch.py -u
+In above cases output_contacts.csv will always be re-created.
+
+* You can fetch mutual groups/contact or degree of a sales person provided that the direct linked url is already present in the output_contacts.csv. But make sure that your input file i.e contacts.csv does not change or remains intact from the last run.
+Command to run> python decrypt_url_data_fetch.py -d
+In this case, the existing output_contacts.csv will be just modified for new column 'Mutual Contacts'
+
+* You ca fetch number of contacts for a sales person provided that direct linked url is already present in the output_contacts.csv. But make sure that your input file i.e contacts.csv remains intact from the last run.
+Command to run> python decrypt_url_data_fetch.py -c
+In this case, the existing output_contacts.csv will be just modified for new column 'Connections'
+
 
 **LinkedIn** 
   * In the terminal type `python follow_li.py` or `python3 follow_li.py`
 
 _Some people's LinkedIn handles may be missing, contain typos or have already been followed. Feedback will be provided in the terminal when running the script._
 
-_
--- decrypt_url_data_fetch.py
+++ decrypt_url_data_fetch.py
@@ -1,4 +1,5 @@
 import time
+import argparse
 import pyperclip
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,19 +8,6 @@ from config import csv_filepath, csv_w_filepath
 import csv
 import os
 
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
-
-if os.path.isfile(csv_w_filepath):
-    os.remove(csv_w_filepath)
-
-driver.get("https://www.linkedin.com/uas/login")
-time.sleep(5)
-driver.maximize_window()
-time.sleep(2)
-driver.find_element_by_name('session_key').send_keys(li_username)
-driver.find_element_by_name('session_password').send_keys(li_password)
-driver.find_element_by_name('session_password').send_keys(Keys.RETURN);
-time.sleep(5)
 csv_data = []
 linkedin_urls = []
 
@@ -27,6 +15,79 @@ linkedin_urls = []
 class Connect:
 
     def __init__(self):
+        self.driver = None
+        self.read_input_csv()
+
+    def setup_driver(self):
+        self.driver = webdriver.Chrome(executable_path=chrome_driver_path)
+        self.driver.get("https://www.linkedin.com/uas/login")
+        time.sleep(5)
+        self.driver.maximize_window()
+        time.sleep(2)
+        self.driver.find_element_by_name('session_key').send_keys(li_username)
+        self.driver.find_element_by_name('session_password').send_keys(li_password)
+        self.driver.find_element_by_name('session_password').send_keys(Keys.RETURN)
+        time.sleep(5)
+
+    def __del__(self):
+        if self.driver:
+            self.driver.close()
+
+    def search_results(self, url):
+        try:
+            self.driver.get(url)
+            time.sleep(15)
+        except Exception as e:
+            print('ERROR: Invalid URL : ---------- ' + url)
+            print(self.driver.current_url)
+            pass
+
+    def get_contact_link(self, url):
+        try:
+            time.sleep(5)
+            action_menu_selector = 'div[class="artdeco-dropdown artdeco-dropdown--placement-bottom artdeco-dropdown--justification-right ember-view"] > button'
+            self.driver.find_element_by_css_selector(action_menu_selector).click()
+            time.sleep(7)
+            self.driver.find_element_by_css_selector('div[data-control-name="copy_linkedin"]').click()
+            time.sleep(6)
+            li_link = pyperclip.paste()
+            print(li_link)
+            return li_link
+        except Exception as e:
+            return f"Problem in getting the Linkedin url for ::{url}"
+
+    def get_second_degree_contact(self, url):
+        try:
+            time.sleep(5)
+            element = self.driver.find_element_by_css_selector('span[class="label-16dp block"]')
+            print(element.text)
+            try:
+                common_text = self.driver.find_element_by_css_selector('div.incommon-entity > dl > dd').text
+                entity = self.driver.find_element_by_css_selector('div.incommon-entity > dl > dt').text
+                if 'groups' in entity:
+                    return common_text.split("in the")[1].strip()
+                elif 'connections' in entity:
+                    return common_text.split("know")[1].strip()
+            except Exception as e:
+                pass
+            try:
+                element1 = self.driver.find_element_by_css_selector('li[class="incommon profile-highlight-card relative overflow-hidden inline-block mr4"] > p')
+                print(f"{element1.text} for {url}")
+                return element.text.strip()
+            except Exception as e:
+                return element.text.strip()
+        except Exception as e:
+            return f"Problem in getting the degree for::{url}"
+
+    def get_connections(self, url):
+        try:
+            element = self.driver.find_element_by_css_selector("div[class='profile-topcard__connections-data type-total inline t-14 t-black--light mr5']")
+            return element.text.split(" ")[0].strip()
+        except Exception as e:
+            return f"Problem in finding the connections for::{url}"
+
+    @staticmethod
+    def read_input_csv():
         with open(csv_filepath, 'r') as myFile:
             try:
                 reader = csv.reader(myFile, delimiter=';', quoting=csv.QUOTE_NONE)
@@ -46,85 +107,119 @@ class Connect:
                     pass
 
     @staticmethod
-    def search_results(url):
-        try:
-            li_url = url
-            driver.get(li_url)
-            time.sleep(15)
-        except Exception as e:
-            print('ERROR: Invalid URL : ---------- ' + li_url)
-            print(driver.current_url)
-            pass
-
-    @staticmethod
-    def get_contact_link(url):
-        try:
-            time.sleep(5)
-            action_menu_selector = 'div[class="artdeco-dropdown artdeco-dropdown--placement-bottom artdeco-dropdown--justification-right ember-view"] > button'
-            driver.find_element_by_css_selector(action_menu_selector).click()
-            time.sleep(7)
-            driver.find_element_by_css_selector('div[data-control-name="copy_linkedin"]').click()
-            time.sleep(6)
-            li_link = pyperclip.paste()
-            print(li_link)
-            return li_link
-        except Exception as e:
-            return f"Problem in getting the Linkedin url for ::{url}"
-
-    @staticmethod
-    def get_second_degree_contact(url):
-        try:
-            time.sleep(5)
-            element = driver.find_element_by_css_selector('span[class="label-16dp block"]')
-            print(element.text)
+    def read_output_csv():
+        output_list = []
+        with open(csv_w_filepath, 'r') as myFile:
             try:
-                common_text = driver.find_element_by_css_selector('div.incommon-entity > dl > dd').text
-                entity = driver.find_element_by_css_selector('div.incommon-entity > dl > dt').text
-                if 'groups' in entity:
-                    return common_text.split("in the")[1].strip()
-                elif 'connections' in entity:
-                    return common_text.split("know")[1].strip()
+                reader = csv.reader(myFile, delimiter=',', quoting=csv.QUOTE_NONE)
             except Exception as e:
-                pass
-            try:
-                element1 = driver.find_element_by_css_selector('li[class="incommon profile-highlight-card relative overflow-hidden inline-block mr4"] > p')
-                print(f"{element1.text} for {url}")
-                return element.text.strip()
-            except Exception as e:
-                pass
-        except Exception as e:
-            return f"Problem in getting the degree for::{url}"
+                reader = csv.reader(myFile)
+            header = next(reader)
+            for row in reader:
+                try:
+                    if len(row) >= 1:
+                        output_dict = {'Contact Url': row[0], 'Mutuals Contacts': row[1], 'Connections': row[2]}
+                        output_list.append(output_dict)
+                    else:
+                        print("Line is blank...")
+                except Exception as e:
+                    pass
+        assert len(output_list), "No data found in output_contacts.csv"
+        return output_list
 
     @staticmethod
-    def get_connections(url):
+    def write_output_csv(csv_data_list):
         try:
-            element = driver.find_element_by_css_selector("div[class='profile-topcard__connections-data type-total inline t-14 t-black--light mr5']")
-            return element.text.split(" ")[0].strip()
-        except Exception as e:
-            return f"Problem in finding the connections for::{url}"
+            fields = ['Contact Url', 'Mutuals Contacts', 'Connections']
+            with open(csv_w_filepath, 'w', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=fields)
+                writer.writeheader()
+                for data in csv_data_list:
+                    try:
+                        writer.writerow(data)
+                    except Exception as e:
+                        data["Contact Url"] = data["Contact Url"].encode('utf-8')
+                        writer.writerow(data)
+        except IOError:
+            print("I/O error")
 
 
 if __name__ == '__main__':
-    cn = Connect()
-    for url in range(len(linkedin_urls)):
-        cn.search_results(linkedin_urls[url])
-        li_link = cn.get_contact_link(linkedin_urls[url])
-        degree = cn.get_second_degree_contact(linkedin_urls[url])
-        connections = cn.get_connections(linkedin_urls[url])
-        dict_data = {'Contact Url': li_link, 'Mutuals Contacts': degree, 'Connections': connections}
-        csv_data.append(dict_data)
-    driver.quit()
+    parser = argparse.ArgumentParser(
+        description='Gather command line arguments'
+    )
+    parser.add_argument(
+        '-u', '--url', action="store_true", help='Fetch Linkedin url only'
+    )
+    parser.add_argument(
+        '-d',
+        '--degree',
+        action="store_true",
+        help="Fetch mutual contacts/groups",
+    )
+    parser.add_argument(
+        '-c',
+        '--connections',
+        action="store_true",
+        help='Fetch number of connections',
+    )
+    args = parser.parse_args()
+    url = args.url
+    degree = args.degree
+    connections = args.connections
 
-    try:
-        fields = ['Contact Url', 'Mutuals Contacts', 'Connections']
-        with open(csv_w_filepath, 'a') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fields)
-            writer.writeheader()
-            for data in csv_data:
-                try:
-                    writer.writerow(data)
-                except Exception:
-                    data["Contact Url"] = data["Contact Url"].encode('utf-8')
-                    writer.writerow(data)
-    except IOError:
-        print("I/O error")
+    cn = Connect()
+    if url and degree and connections:
+        cn.setup_driver()
+        if os.path.isfile(csv_w_filepath):
+            os.remove(csv_w_filepath)
+        for url in range(len(linkedin_urls)):
+            cn.search_results(linkedin_urls[url])
+            li_link = cn.get_contact_link(linkedin_urls[url])
+            degree = cn.get_second_degree_contact(linkedin_urls[url])
+            connections = cn.get_connections(linkedin_urls[url])
+            dict_data = {'Contact Url': li_link, 'Mutuals Contacts': degree, 'Connections': connections}
+            csv_data.append(dict_data)
+        cn.write_output_csv(csv_data)
+
+    elif url and not degree and not connections:
+        cn.setup_driver()
+        if os.path.isfile(csv_w_filepath):
+            os.remove(csv_w_filepath)
+        for url in range(len(linkedin_urls)):
+            cn.search_results(linkedin_urls[url])
+            li_link = cn.get_contact_link(linkedin_urls[url])
+            dict_data = {'Contact Url': li_link, 'Mutuals Contacts': None, 'Connections': None}
+            csv_data.append(dict_data)
+        cn.write_output_csv(csv_data)
+
+    elif degree and not url and not connections:
+        if not os.path.isfile(csv_w_filepath):
+            assert False, "No output file found to append mutual contacts/groups"
+        url_data = cn.read_output_csv()
+        cn.setup_driver()
+        for url in range(len(linkedin_urls)):
+            cn.search_results(linkedin_urls[url])
+            li_link = url_data[url]['Contact Url']
+            degree = cn.get_second_degree_contact(linkedin_urls[url])
+            connections = url_data[url]['Connections']
+            dict_data = {'Contact Url': li_link, 'Mutuals Contacts': degree, 'Connections': connections}
+            csv_data.append(dict_data)
+        cn.write_output_csv(csv_data)
+
+    elif connections and not url and not degree:
+        if not os.path.isfile(csv_w_filepath):
+            assert False, "No output file found to append mutual contacts/groups"
+        url_data = cn.read_output_csv()
+        cn.setup_driver()
+        for url in range(len(linkedin_urls)):
+            cn.search_results(linkedin_urls[url])
+            li_link = url_data[url]['Contact Url']
+            degree = url_data[url]['Mutuals Contacts']
+            connections = cn.get_connections(linkedin_urls[url])
+            dict_data = {'Contact Url': li_link, 'Mutuals Contacts': degree, 'Connections': connections}
+            csv_data.append(dict_data)
+        cn.write_output_csv(csv_data)
+
+    else:
+        print("Please enter the valid combination of command line arguments")
